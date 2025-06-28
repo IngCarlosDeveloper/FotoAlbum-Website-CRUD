9945729472d3774483136766067c5f3abc8d8328
IsMrGabox <angelgabrieljimenez2308@gmail.com>
2025-06-28 19:14:50 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -11,5 +11,6 @@
         Clave: <input id="clave" type="password" name="clave" required><br>
      <input type="submit" value="Entrar">
 </form>
+<p><a href="registro.php">Registrate</a></p>
 </body>
 </html>
